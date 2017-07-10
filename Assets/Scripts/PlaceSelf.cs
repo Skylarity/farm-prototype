1354d15f04ae14da278616cf04331b578faeb028
Skyler Rexroad <skyrex1095@gmail.com>
2017-07-09 23:32:40 -0600

    Initial project

---
+++ Assets/Scripts/PlaceSelf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceSelf : MonoBehaviour {
+
+	void Start () {
+		Debug.Log(this.transform.position);
+
+		RaycastHit hit;
+		Vector3 down = new Vector3(0, -1, 0);
+		if (Physics.Raycast(this.transform.position, down, out hit)) {
+			float distanceToGround = hit.distance;
+			this.transform.position = new Vector3(transform.position.x, transform.position.y - distanceToGround, transform.position.z);
+
+			Debug.Log(this.transform.position);
+		}
+	}
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+	public Transform cube;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit)) {
+				if (hit.collider.tag == "Ground") {
+					float offset = 1000f;
+					Vector3 position = new Vector3(Mathf.Floor(hit.point.x + 0.5f), offset, Mathf.Floor(hit.point.z + 0.5f));
+					Instantiate(cube, position, Quaternion.identity);
+				} else {
+					Destroy(hit.transform.gameObject);
+				}
+			}
+		}
+	}
+}
